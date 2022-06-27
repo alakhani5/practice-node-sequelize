7cc6824e165800b8a9317a5d5f904bb228477407
Amaan Lakhani <amaan.lakhani5@gmail.com>
2022-06-27 11:31:34 -0400

    adding a dependency for express for others

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
-// const express = require("express");
-// const router = express.Router();
+const express = require("express");
+const router = express.Router();
+
 // const {
 //   reset,
 //   listPeople,
